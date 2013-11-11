f5d2308a7a12e843c987439d574d56dd73c71d0d
James Newton <james@Zaphyous.com>
2013-11-11 17:32:27 -0600

    add nprogress for useless but attractive loading bar. yeah. i said it.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require nprogress
+
+$(document).on('page:fetch', function() { NProgress.start(); });
+$(document).on('page:change', function() { NProgress.done(); });
+$(document).on('page:restore', function() { NProgress.remove(); });
