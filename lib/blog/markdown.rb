acc93f77b07b846d2056b09a15628c111e475133
James Newton <james@Zaphyous.com>
2013-11-11 17:27:19 -0600

    style the code blocks as dictated by the design

--- lib/blog/markdown.rb
+++ lib/blog/markdown.rb
@@ -1,6 +1,6 @@
 class Blog::Markdown
   def initialize(content)
-    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, prettify: true)
+    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::BlogHTML, autolink: true, fenced_code_blocks: true)
     @content  = content
   end
 
--- lib/redcarpet/render/blog_html.rb
+++ lib/redcarpet/render/blog_html.rb
@@ -1,5 +1,9 @@
 class Redcarpet::Render::BlogHTML < Redcarpet::Render::HTML
   def block_code(content, language)
-    Pygments.highlight(code, lexer: language)
+    <<-HTML
+      <pre><code>
+#{Pygments.highlight(content, lexer: language).gsub(/(\A\<div class=\"highlight\">|\<\/div\>\z)/, '').gsub(/(\A\<pre\>|\<\/pre\>\z)/, '').chomp}
+      </code></pre>
+    HTML
   end
 end
