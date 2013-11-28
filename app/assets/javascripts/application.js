98dc0db6f99b4e4ec8cfbc7d0159ab2530a7197e
James Newton <james@Zaphyous.com>
2013-11-27 23:19:58 -0600

    don't show NProgress spinner

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require nprogress
 //= require devise
 
+NProgress.configure({ showSpinner: false })
 $(document).on('page:fetch', function() { NProgress.start(); });
 $(document).on('page:change', function() { NProgress.done(); });
 $(document).on('page:restore', function() { NProgress.remove(); });
