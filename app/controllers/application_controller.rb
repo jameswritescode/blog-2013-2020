ef5117337f717f45e8530fa9dc1e7b8307efbf9e
James Newton <james@Zaphyous.com>
2013-11-27 23:56:41 -0600

    wip - start routing admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
   end
+
+  def after_sign_in_path_for(resource)
+    admin_root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Blog::Application.routes.draw do
   devise_for :users
+
+  namespace :admin do
+    resources :posts
+
+    root to: 'dashboard#index'
+  end
+
   get '/:id', to: 'posts#show', as: :post
 
   root to: 'posts#index'
