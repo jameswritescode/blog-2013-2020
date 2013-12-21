7a2f92ab4b3011cd0d81346819cb094a12a63330
James Newton <james@Zaphyous.com>
2013-12-20 18:41:20 -0600

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(
+  email: 'hello@jamesnewton.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+FactoryGirl.create(:post)
+
+FactoryGirl.create(:post,
+  content: <<-MARKDOWN
+# #{Faker::Lorem.words(5).join(' ')}
+## #{Faker::Lorem.words(5).join(' ')}
+### #{Faker::Lorem.words(5).join(' ')}
+#### #{Faker::Lorem.words(5).join(' ')}
+##### #{Faker::Lorem.words(5).join(' ')}
+###### #{Faker::Lorem.words(5).join(' ')}
+
+`Ut tenetur tempora perferendis.` Maiores vitae et voluptatem natus. "Pariatur eos ab dignissimos qui exercitationem." Voluptate quidem reiciendis corporis nesciunt et occaecati's repellat qui's. Repudiandae ea temporibus ut odio quam nihil veritatis. Enim et vero ipsum sed. Esse id blanditiis neque ipsa molestiae laborum omnis sed. Debitis magnam voluptates qui quia. Ut earum incidunt exercitationem possimus harum. Vel velit enim odit assumenda recusandae possimus. *Ipsa saepe voluptas impedit repellendus in vel quo et.*
+
+> **Ut tenetur tempora perferendis.** Maiores vitae et voluptatem natus. Pariatur eos ab dignissimos qui exercitationem. Voluptate quidem reiciendis corporis nesciunt et occaecati repellat qui. Repudiandae ea temporibus ut odio quam nihil veritatis. Enim et vero ipsum sed. Esse id blanditiis neque ipsa molestiae laborum omnis sed. Debitis magnam voluptates qui quia. Ut earum incidunt exercitationem possimus harum. Vel velit enim odit assumenda recusandae possimus. Ipsa saepe voluptas impedit repellendus in vel quo et.
+
+[Ut tenetur tempora perferendis](#). Maiores vitae et voluptatem natus. Pariatur eos ab dignissimos qui exercitationem. Voluptate quidem reiciendis corporis nesciunt et occaecati repellat qui. Repudiandae ea temporibus ut odio quam nihil veritatis. Enim et vero ipsum sed. Esse id blanditiis neque ipsa molestiae laborum omnis sed. Debitis magnam voluptates qui quia. Ut earum incidunt exercitationem possimus harum. Vel velit enim odit assumenda recusandae possimus. Ipsa saepe voluptas impedit repellendus in vel quo et.
+  MARKDOWN
+)
+
+FactoryGirl.create(:post,
+  content: <<-RUBY
+#{Faker::Lorem.paragraph}
+
+```ruby
+class Person
+  def intitalize(first_name, last_name)
+    @first_name = first_name
+    @last_name  = last_name
+  end
+
+  def name
+    "\#{@first_name} \#{@last_name}"
+  end
+end
+```
+
+#{Faker::Lorem.paragraph}
+  RUBY
+)
