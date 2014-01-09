afbfaa6e5570d32801c93cfc9727186d1ac2f425
James Newton <james@Zaphyous.com>
2014-01-09 17:40:40 -0600

    remove secret token and generate a random one

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,17 @@
-# Be sure to restart your server when you modify this file.
+require 'securerandom'
 
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
+def find_secure_token
+  token_file = Rails.root.join('.secret')
 
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
+  if File.exist?(token_file)
+    File.read(token_file).chomp
+  else
+    token = SecureRandom.hex(64)
 
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Blog::Application.config.secret_key_base = 'a26b6831fd00b8e2aa97db207aff36f78f89faa838e8b91da92b7861d4e684f118dd496103bd319b8c9dc1e18c399b5d0b76ede1006cf52c5da26a2a9d033aae'
+    File.write(token_file, token)
+
+    token
+  end
+end
+
+Blog::Application.config.secret_key_base = find_secure_token
